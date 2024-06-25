ea28845ca44ca8dc72677c479195609e50cfbf22
toilahieu1410 <34303013+toilahieu1410@users.noreply.github.com>
2024-06-26 06:07:56 +0700

    update services boardService.js

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -5,14 +5,16 @@
  */
 
 import { StatusCodes } from 'http-status-codes'
-import ApiError from '~/utils/ApiError'
+import { boardService } from '~/services/boardService'
 
 const createNew = async (req, res, next) => {
   try {
+
     //Điều hướng dữ liệu sang tầng Service
-    // throw new ApiError('minhhieu test error')
+    const createdBoard = await boardService.createNew(req.body)
+
     // Có kết quả thì trả về phía Client
-    res.status(StatusCodes.CREATED).json({message: 'POST from Validation: API create new board'})
+    res.status(StatusCodes.CREATED).json(createdBoard)
   } catch (error) {
     next(error)
     // res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
